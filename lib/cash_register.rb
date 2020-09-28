9496f4a2cabf21ac17b7149108d78fd026fc5b14
Anthony Leon <acidknights89@gmail.com>
2020-09-28 23:08:36 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,8 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     self.total += price * quantity
     quantity.times do 
-      items << 
+      items << title
+      
   
   
 end
