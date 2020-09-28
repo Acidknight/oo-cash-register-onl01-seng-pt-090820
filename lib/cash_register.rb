24a2964e3d4449711e7bdca78c8b74cc5c71de19
Anthony Leon <acidknights89@gmail.com>
2020-09-28 23:14:48 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,22 +1,21 @@
 class CashRegister
   
- attr_accessor :items, :discount, :total, :last_transaction
-
-  def initialize(discount=0)
-    @total = 0
+  attr_accessor :total, :discount, :items, :last_transaction
+  
+  def initialize(total = 0)
+    @total = total
     @discount = discount
     @items = []
   end
-
-  def add_item(title, amount, quantity=1)
-    self.total += amount * quantity
-    quantity.times do
+  
+  def add_item(title, price, quantity = 1)
+    self.total += price * quantity
+    quantity.times do 
       items << title
     end
-    self.last_transaction = amount * quantity
   end
-
-  def apply_discount
+  
+   def apply_discount
     if discount != 0
       self.total = (total * ((100.0 - discount.to_f)/100)).to_i
       "After the discount, the total comes to $#{self.total}."
@@ -28,4 +27,7 @@ class CashRegister
   def void_last_transaction
     self.total = self.total - self.last_transaction
   end
+
+  
+  
 end
